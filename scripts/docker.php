108003f7fff7300ad175b05a47c968c298781535
Philipp Schüle <p.schuele@metaways.de>
2019-03-28 13:12:04 +0100

    tweak(docker.php): improve php startup script
    all compose configurations in the compose/ subdir can be used now

--- scripts/docker.php
+++ scripts/docker.php
@@ -2,32 +2,11 @@
 array_shift($argv);
 $command = 'docker-compose -f docker-compose.yml ';
 foreach ($argv as $arg) {
-    switch ($arg):
-        case('clamav'):
-            $command .= '-f compose/clamav.yml ';
-            break;
-        case('confroom'):
-            $command .= '-f compose/confroom.yml ';
-            break;
-        case('docservice'):
-            $command .= '-f compose/docservice.yml ';
-            break;
-        case('mail'):
-            $command .= '-f compose/mail.yml ';
-            break;
-        case('pma'):
-            $command .= '-f compose/pma.yml ';
-            break;
-        case('webpack'):
-            $command .= '-f compose/webpack.yml ';
-            break;
-        case('worker'):
-            $command .= '-f compose/worker.yml ';
-            break;
-        case('xdebug'):
-            $command .= '-f compose/xdebug.yml ';
-            break;
-    endswitch;
+    // strip some unwanted chars
+    $composeFile = preg_replace('/[^\w^_^\s]/', '', escapeshellarg($arg));
+    $filename = 'compose/' . $composeFile . '.yml';
+    if (file_exists($filename)) {
+        $command .= '-f ' . $filename . ' ';
+    }
 }
 system($command . ' up');
-?>
