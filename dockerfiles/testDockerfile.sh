b59e199c2d9f2543f4bbefbbbe11553197f8aaa5
Milan Mertens <m.mertens@metaways.de>
2019-11-19 10:09:00 +0100

    fix: test

--- dockerfiles/testDockerfile.sh
+++ dockerfiles/testDockerfile.sh
@@ -1,4 +1,4 @@
-phpinfo=$(docker-compose run web sh -c "php -info")
+phpinfo=$(docker-compose run web sh -c "php -m")
 
 for i in igbinary xdebug yaml sockets pcntl intl bcmath; do
   if [[ $phpinfo != *"$i"* ]]; then
