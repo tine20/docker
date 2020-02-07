267cf27b630e41aae2a492e53d043ee90d8ef846
Philipp Schüle <p.schuele@metaways.de>
2020-02-07 10:31:19 +0100

    feature(mailstack): add smtp_virtual_domains table
    ... with instancename
    
    see #165441: [Tine 2.0/Postfix] instancename in die smtp_virtual_domains

--- dockerfiles/mailstack/control/files/postfix_init.sql
+++ dockerfiles/mailstack/control/files/postfix_init.sql
@@ -1,29 +1,38 @@
- --
- -- Table structure for table `smtp_users`
- --
+--
+-- Table structure for table `smtp_users`
+--
 
- CREATE TABLE IF NOT EXISTS `smtp_users` (
-   `userid` varchar(40) NOT NULL,
-   `client_idnr` varchar(40) NOT NULL,
-   `username` varchar(80) NOT NULL,
-   `passwd` varchar(256) NOT NULL,
-   `email` varchar(80) DEFAULT NULL,
-   `forward_only` tinyint(1) NOT NULL DEFAULT 0,
-   PRIMARY KEY (`userid`, `client_idnr`),
-   UNIQUE KEY `username` (`username`),
-   UNIQUE KEY `email` (`email`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `smtp_users` (
+  `userid` varchar(40) NOT NULL,
+  `client_idnr` varchar(40) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `passwd` varchar(256) NOT NULL,
+  `email` varchar(80) DEFAULT NULL,
+  `forward_only` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`userid`, `client_idnr`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
- --
- -- Table structure for table `smtp_destinations`
- --
+--
+-- Table structure for table `smtp_destinations`
+--
 
- CREATE TABLE IF NOT EXISTS `smtp_destinations` (
-   `userid` VARCHAR( 40 ) NOT NULL ,
-   `source` VARCHAR( 80 ) NOT NULL ,
-   `destination` VARCHAR( 80 ) NOT NULL ,
-   `dispatch_address` tinyint(1) NOT NULL DEFAULT 1,
-   CONSTRAINT `smtp_destinations::userid--smtp_users::userid` FOREIGN KEY (`userid`)
-   REFERENCES `smtp_users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
-   ) ENGINE=Innodb DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `smtp_destinations` (
+  `userid` VARCHAR( 40 ) NOT NULL ,
+  `source` VARCHAR( 80 ) NOT NULL ,
+  `destination` VARCHAR( 80 ) NOT NULL ,
+  `dispatch_address` tinyint(1) NOT NULL DEFAULT 1,
+  CONSTRAINT `smtp_destinations::userid--smtp_users::userid` FOREIGN KEY (`userid`)
+  REFERENCES `smtp_users` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=Innodb DEFAULT CHARSET=utf8;
 
+--
+-- Table structure for table `smtp_virtual_domains`
+--
+
+CREATE TABLE `smtp_virtual_domains` (
+  `domain` varchar(50) NOT NULL,
+  `instancename` varchar(40) NOT NULL,
+  PRIMARY KEY (`domain`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
--- dockerfiles/mailstack/control/files/postfixmultiinstance_init.sql
+++ dockerfiles/mailstack/control/files/postfixmultiinstance_init.sql
@@ -38,3 +38,13 @@ KEY `source` (`source`)
 --
 ALTER TABLE `smtp_destinations`
 ADD CONSTRAINT `smtp_destinations_ibfk_1` FOREIGN KEY (`users_id`) REFERENCES `smtp_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Table structure for table `smtp_virtual_domains`
+--
+
+CREATE TABLE `smtp_virtual_domains` (
+  `domain` varchar(50) NOT NULL,
+  `instancename` varchar(40) NOT NULL,
+  PRIMARY KEY (`domain`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
