3e63e74dfe1b282e74f6d17bb41eca19b2f4c62e
Milan Mertens <m.mertens@metaways.de>
2020-02-12 10:21:18 +0100

    fix: set previewService version to tow

--- configs/conf.d/docservice.inc.php
+++ configs/conf.d/docservice.inc.php
@@ -3,6 +3,7 @@
 return array(
     'filesystem' => array(
         'createPreviews' => true,
-        'previewServiceUrl' => 'http://document-preview/v2/documentPreviewService',
+	'previewServiceVersion' => 2,
+	'previewServiceUrl' => 'http://document-preview/v2/documentPreviewService',
     ),
 );
