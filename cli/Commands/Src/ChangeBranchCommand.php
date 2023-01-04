87b3136590c514172f472190990bd1c675872179
Philipp Schüle <p.schuele@metaways.de>
2023-01-04 17:57:13 +0100

    feature(console/ChangeBranch): restart web container for db prefix change
    ... and some code cleanup
    
    see #4304 switch db prefix depending on branch

--- cli/Commands/Src/ChangeBranchCommand.php
+++ cli/Commands/Src/ChangeBranchCommand.php
@@ -5,16 +5,14 @@ namespace App\Commands\Src;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use App\ConsoleStyle;
 
-use App\Commands\BaseCommand;
+use App\Commands\Docker\DockerCommand;
 use App\Commands\Docker\DockerNpmInstallCommand;
 use App\Commands\Docker\DockerWebpackRestartCommand;
 use App\Commands\Tine\TineClearCacheCommand;
 
-class ChangeBranchCommand extends BaseCommand {
+class ChangeBranchCommand extends DockerCommand {
 
     protected function configure()
     {
@@ -23,26 +21,29 @@ class ChangeBranchCommand extends BaseCommand {
         $this
             ->setName('src:changeBranch')
             ->setDescription('change dev branch')
-            ->setHelp('')
-            ->addArgument(
-                'branch',
-                InputArgument::REQUIRED,
-                'target branch')
-        ;
+            ->setHelp('');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $io = new ConsoleStyle($input, $output);
-        $targetBranch = $input->getArgument('branch');
+        parent::execute($input, $output);
 
         // @TODO optionally check out git code?
         // @TODO optionally update git submodules
         // @TODO start with a base? e.g. metaways/2023.11 can start with 2023.11 cache?
 
-        $this->composerInstall($targetBranch);
+        // stop tine/web container to make sure DB PREFIX is correct & tine20_install is executed
+        // restart/up is done in DockerWebpackRestartCommand
+        $io = new ConsoleStyle($input, $output);
+        $io->info('Stopping web container ...');
+        $composeString = $this->getComposeString();
+        passthru($composeString . " stop web");
 
-        $this->npmInstall($targetBranch);
+        $io->info('Running composer install ...');
+        $this->composerInstall($this->branch);
+
+        $io->info('Running npm install ...');
+        $this->npmInstall($this->branch);
 
         (new DockerWebpackRestartCommand())->execute($input, $output);
         (new TineClearCacheCommand())->execute($input, $output);
@@ -55,11 +56,11 @@ class ChangeBranchCommand extends BaseCommand {
     {
         $cacheDir = $this->baseDir . "/cache/composer/$targetBranch";
         echo "cacheDir: $cacheDir\n";
-        // @TODO compute basebranch and prefill if cahceDir is empty
+        // @TODO compute basebranch and prefill if cacheDir is empty
 
         `mkdir -p $cacheDir`;
         `cp $this->srcDir/tine20/composer.* $cacheDir`;
-        `cd $cacheDir && composer install`;
+        `cd $cacheDir && composer install --ignore-platform-reqs`;
 
         `rsync -a --delete $cacheDir/vendor $this->srcDir/tine20`;
     }
@@ -68,11 +69,11 @@ class ChangeBranchCommand extends BaseCommand {
     {
         $cacheDir = $this->baseDir . "/cache/node/$targetBranch";
         echo "cacheDir: $cacheDir\n";
-        // @TODO compute basebranch and prefill if cahceDir is empty
+        // @TODO compute basebranch and prefill if cacheDir is empty
 
         `mkdir -p $cacheDir`;
         `cp $this->srcDir/tine20/Tinebase/js/package.* $cacheDir`;
-//        `cp $this->srcDir/tine20/Tinebase/js/npm-* $cacheDir`;
+        `cp $this->srcDir/tine20/Tinebase/js/npm-* $cacheDir`;
 
         DockerNpmInstallCommand::runNpmInstall($cacheDir, $targetBranch);
         `rsync -a --delete $cacheDir/node_modules $this->srcDir/tine20/Tinebase/js`;
